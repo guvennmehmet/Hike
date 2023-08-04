ca08ccd5f0f6b3164682f1c2362b304365f22d98
Mehmet Güven <guvennmehmet37@gmail.com>
2023-08-04 22:44:29 +0300

    card component

---
+++ Hike/Component/CardView.swift
@@ -0,0 +1,45 @@
+//
+//  CardView.swift
+//  Hike
+//
+//  Created by Mehmet Güven on 4.08.2023.
+//
+
+import SwiftUI
+
+struct CardView: View {
+    var body: some View {
+        
+        // MARK: - CARD
+        
+        ZStack {
+            CustomBackgroundView()
+            
+            ZStack {
+                Circle()
+                    .fill(
+                        LinearGradient(
+                            colors: [
+                                Color("ColorIndigoMedium"),
+                                Color("ColorSalmonLight")
+                                    ],
+                            startPoint: .topLeading,
+                            endPoint: .bottomTrailing
+                    )
+                )
+                    .frame(width: 256, height: 256)
+                
+                Image("image-1")
+                    .resizable()
+                    .scaledToFit()
+            }
+        } //: CARD
+        .frame(width: 320, height: 570)
+    }
+}
+
+struct CardView_Previews: PreviewProvider {
+    static var previews: some View {
+        CardView()
+    }
+}
