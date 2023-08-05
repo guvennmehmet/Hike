9667b9c3488f1b93fd1b4d592cc51b00958dacf9
Mehmet Güven <guvennmehmet37@gmail.com>
2023-08-05 15:07:41 +0300

    SwiftUI Button Style

---
+++ Hike/Utility/GradientButtonStyle.swift
@@ -0,0 +1,25 @@
+//
+//  GradientButtonStyle.swift
+//  Hike
+//
+//  Created by Mehmet Güven on 5.08.2023.
+//
+
+import Foundation
+import SwiftUI
+
+struct GradientButton: ButtonStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        configuration
+            .label
+            .padding(.vertical)
+            .padding(.horizontal, 30)
+            .background(
+                configuration.isPressed ?
+                LinearGradient(colors: [.customGrayMedium, .customGrayLight], startPoint: .top, endPoint: .bottom)
+                :
+                LinearGradient(colors: [.customGrayLight, .customGrayMedium], startPoint: .top, endPoint: .bottom)
+            )
+            .cornerRadius(40)
+    }
+}
